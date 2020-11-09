a844dbf5e6f9c14646739a81635ae0aa04c2193f
Gene Gotimer <eugene.gotimer@steampunk.com>
2020-11-09 17:26:17 -0500

    Reformatted with Google Code Style, newlines at eof

--- src/main/java/com/steampunk/shapes/Rectangle.java
+++ src/main/java/com/steampunk/shapes/Rectangle.java
@@ -3,19 +3,16 @@ package com.steampunk.shapes;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * A rectangle, i.e., a 4-sided polygon with right angles and opposite sides equal length.
- */
+/** A rectangle, i.e., a 4-sided polygon with right angles and opposite sides equal length. */
 public class Rectangle extends Quadrilateral {
 
-    private static final List<Double> RIGHT_ANGLES = Arrays.asList(90.0d, 90.0d, 90.0d, 90.0d);
+  private static final List<Double> RIGHT_ANGLES = Arrays.asList(90.0d, 90.0d, 90.0d, 90.0d);
 
-    public Rectangle(final String shape, final List<Double> lengths) {
-        super(shape, lengths, RIGHT_ANGLES);
-    }
+  public Rectangle(final String shape, final List<Double> lengths) {
+    super(shape, lengths, RIGHT_ANGLES);
+  }
 
-    public Rectangle(final List<Double> lengths) {
-        this("rectangle", lengths);
-    }
-
+  public Rectangle(final List<Double> lengths) {
+    this("rectangle", lengths);
+  }
 }
--- src/main/java/com/steampunk/shapes/package-info.java
+++ src/main/java/com/steampunk/shapes/package-info.java
@@ -1,4 +1,2 @@
-/**
-* Sample code working with an object hierarchy. It isn't as easy as it looks.
-*/
+/** Sample code working with an object hierarchy. It isn't as easy as it looks. */
 package com.steampunk.shapes;
